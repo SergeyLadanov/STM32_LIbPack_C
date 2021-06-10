e28cbce57e8626b55eb43083ce01a718151e8163
Sergey <61871201+SergeyLadanov@users.noreply.github.com>
2021-06-10 12:48:34 +0300

    Create stringutils.h

---
+++ StringUtils/stringutils.h
@@ -0,0 +1,15 @@
+#ifndef __STRINGUTILS_H_
+#define __STRINGUTILS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ 
+char* str_split(char** stringp, const char* delim);
+  
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
