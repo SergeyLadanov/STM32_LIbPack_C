a3a812ecbcc63e37c81927ecdc082d5dabda6393
Sergey <61871201+SergeyLadanov@users.noreply.github.com>
2021-06-10 12:46:03 +0300

    Create stringutils.c

---
+++ StringUtils/stringutils.c
@@ -0,0 +1,22 @@
+#include "stringutils.h"
+
+// Аналог функции strsep (деление строки на токены, в том числе нулевой длины)
+char* str_split(char** stringp, const char* delim)
+{
+  char* start = *stringp;
+  char* p;
+
+  p = (start != NULL) ? strpbrk(start, delim) : NULL;
+
+  if (p == NULL)
+  {
+    *stringp = NULL;
+  }
+  else
+  {
+    *p = '\0';
+    *stringp = p + 1;
+  }
+
+  return start;
+}
